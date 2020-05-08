6174d46ec38744336690497fb65104a006a10cba
Jonah Aitchison <jonah@pop-os.localdomain>
2020-05-08 13:00:59 -0500

    one more attempt

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,21 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    result = []
+    num_hash = {}
+
+    # print(a)
+
+    for num in a:
+    	fixed_num = abs(num)
+
+    	if fixed_num not in num_hash:
+    		num_hash[fixed_num] = 0
+    	elif fixed_num in num_hash:
+    		num_hash[fixed_num] += 1
+
+    	if num_hash[fixed_num] > 0:
+    		result.append(fixed_num)
 
     return result
 
